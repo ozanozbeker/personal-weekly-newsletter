f60c314e7781d3554c71310aa510944a0648dba1
Ozan Ozbeker <ozanozbeker99@outlook.com>
2024-12-01 22:44:06 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# personal-weekly-newsletter
-Cal Newport discusses the concept of creating your own Personal Weekly Newsletter in his book Digital Minimalism. I like the idea so this is my effort to implement it in my effort of living the "deep life".
+# Personal Weekly Newsletter
+
+## Background
+
+Cal Newport discusses the concept of creating your own personal weekly newsletter in his podcast, [Deep Questions](https://www.thedeeplife.com/listen/). I liked the idea so this is my effort to implement it in my effort of living the "deep life".
+
+This single document project scrapes my favorite blogs to see if there are new posts, and sends myself a weekly email notifying which blogs have new posts, with a link.
+
+## Scraping the Blogs
+
+The blogs I'm currently reading are listed in `blogs.csv`. While I call it blogs, some authors also have talks and projects that I'm also interested in.
+
+Instead of scraping the listings page page of each blog, which can vary across blogs, this script just looks at the sitemap of the blogs to see if new articles have been created since the previous week. Sitemaps can usually be found in the `sitemap.xml` or `sitemap_index.xml` file. I keep a track of the posts that have been in my newsletter already in `scraped_urls.csv`. 
+
+-   I was originally going to use the timestamp/lastmod/etc., but while going through different blogs I noticed that the lastmod changes for posts very differently based on how the website is publish, so using the mod time is not a reliable method. For now,  the `scraped_urls.csv` will work and maybe I'll come up with a better method later if the file becomes too big. The biggest downfall of this method is that the first time a blog is scraped, every article in its history is added to the newsletter, but it's not the end of the world.
+
+## Build & Send Email
+
+[{gt}](https://gt.rstudio.com/) will create an HTML table which will be the body of the email, [{blastula}](https://rstudio.github.io/blastula/index.html) will send the email using my Gmail account for SMTP, & GitHub actions will create a runner weekly to run the code.
---
+++ personal-weekly-newsletter.R
@@ -0,0 +1,108 @@
+# Initialize ----
+library(blastula) # Build email
+library(stringr)  # String Manipulation
+library(dplyr)    # Data Manipulation
+library(purrr)    # Functional Programming
+library(readr)    # Data Read/Write
+library(httr2)    # HTTP Requests
+library(xml2)     # XML Processing
+library(gt)       # HTML Tables
+
+# Scrape Blogs ----
+blogs = read_csv("blogs.csv", col_types = "ccc")
+scraped_urls = read_csv("scraped_urls.csv", col_types = "c")
+
+reqs = blogs |> 
+  pull(host) |> 
+  str_c("/sitemap.xml") |> 
+  map(request)
+
+resps = req_perform_sequential(reqs, on_error = "continue")
+
+extract_new_posts = function(resp) {
+
+  site = str_remove(resp_url(resp), "/sitemap.xml")
+
+  content = blogs |> 
+    filter(host == site) |> 
+    pull(content)
+
+  regex_filter = str_c(site, "/", str_split(content, "\\|")[[1]], "/.", collapse = "|")
+
+  xml = resp_body_xml(resp)
+  ns = xml_ns(xml)
+
+  urls_with_lastmod = xml_find_all(xml, ".//d1:url", ns = ns) # might have to switch "d1" with `names(ns[1])`
+
+  results = site |> 
+    tibble(
+      url = urls_with_lastmod |> 
+        xml_find_all(".//d1:loc", ns = ns) |> 
+        xml_text()
+    ) |>
+    filter(
+      !url %in% scraped_urls$url,
+      str_starts(url, regex_filter)
+    ) |> 
+    mutate(
+      post = url |> 
+        str_remove("/index[.]html") |> 
+        str_remove(str_remove_all(regex_filter, "/[.](?=\\|)|/[.]$")) |> 
+        str_replace_all("/", "-") |> 
+        str_remove("\\d{4}-\\d{2}-\\d{2}") |> 
+        str_replace_all("-", " ") |> 
+        str_squish() |> 
+        str_to_title(),
+      post = if_else(post == "", "Post name failed to parse", post)
+    )
+    
+  return(results)
+}
+
+results = resps_successes(resps) |> 
+  map(extract_new_posts) |> 
+  list_rbind() |> 
+  left_join(blogs |> select(-content), join_by(site == host))
+
+results |> 
+  select(url) |> 
+  union(scraped_urls) |> 
+  write_csv("scraped_urls.csv")
+
+# Build Email ----
+table = results |> 
+  select(author, post, url) |> 
+  gt() |> 
+  tab_header(
+    title = "Personal Weekly Newsletter",
+    subtitle = format(Sys.Date(), "%B %m, %Y")
+  ) |> 
+  fmt_url(
+    columns = url,
+    label = from_column("post"),
+    show_underline = FALSE
+  ) |> 
+  cols_hide(post) |>
+  cols_label(
+    author = "Author",
+    url = "Post"
+  ) |> 
+  cols_width(
+    author ~ px(125),
+    url ~ px(475)
+  )
+
+# Send Email ----
+smtp_send(
+  email = compose_email(as_raw_html(table)),
+  subject = str_c("Personal Weekly Newsletter | ", format(Sys.Date(), "%B %m, %Y")),
+  to = Sys.getenv("OUTLOOK_EMAIL"),
+  from = Sys.getenv("SMTP_USER"),
+  credentials = creds_envvar(
+    user = Sys.getenv("SMTP_USER"),
+    pass_envvar = "SMTP_PASSWORD",
+    host = Sys.getenv("SMTP_HOST"),
+    port = Sys.getenv("SMTP_PORT"),
+    use_ssl = TRUE
+  )
+)
